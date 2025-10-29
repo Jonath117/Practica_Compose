ee47a9a3e7ceb713aefc278ee06b573f1c3fd747
Jonathan <rochacontrerasjonathan40@gmail.com>
2025-10-29 15:59:54 -0400

    Ejercicio hecho en vivo en clases

--- app/src/main/java/com/example/layoutcomposeexample/MainActivity.kt
+++ app/src/main/java/com/example/layoutcomposeexample/MainActivity.kt
@@ -4,23 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.layoutcomposeexample.ui.theme.LayoutComposeExampleTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +39,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    RowExample()
                 }
             }
         }
@@ -46,6 +53,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Composable
+fun RowExample(){
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(top = 150.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Text(text = "elemento 1")
+        Text(text = "elemento 2")
+        Text(text = "elemento 3")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
